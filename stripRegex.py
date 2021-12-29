9dd1bc7cf68754b668c11590e0d085092aa404f5
Gustavo <gugas_soares@hotmail.com>
2021-12-29 17:14:07 -0300

    add stripRegex.py

---
+++ stripRegex.py
@@ -0,0 +1,13 @@
+import re
+'''RegEx version of the built-in function strip()
+Used to remove whitespaces at the start/end of a string'''
+
+x = input('Insert a string for strip(): ')
+
+def regex_strip(string):
+
+    pattern = '(^\s*)|(\s*$)'
+    regex = re.sub(pattern, '', string)
+    return regex
+
+print(regex_strip(x))
