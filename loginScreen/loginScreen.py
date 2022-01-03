38d1198641e063010ea3f88af08a0df4a52d1dcf
Gustavo <gugas_soares@hotmail.com>
2022-01-03 17:07:51 -0300

    add loginScreen dir

---
+++ loginScreen/loginScreen.py
@@ -0,0 +1,9 @@
+#simple login screen
+
+#TODO: User registration function
+
+#TODO: Strong password detector
+
+#TODO: Check if user exists or not
+
+#TODO: Assign user to a text file or a module, or a database haven't decided yet
