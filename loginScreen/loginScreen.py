aab1ab6f68cda14f4d8cbd67cdfa353e55d0c4f8
Gustavo <gugas_soares@hotmail.com>
2022-01-04 18:59:05 -0300

    update loginScreen.py

--- loginScreen/loginScreen.py
+++ loginScreen/loginScreen.py
@@ -6,43 +6,43 @@ emails = []
 passwords = []
 users_shelf = shelve.open('Users')
 
-if __name__ == '__main__':
 # Register user.
-    def ask_user(message=None):
-        global user_email, user_password
+def ask_user(message=None):
+    global user_email, user_password
         
-        if message is None:
-            user_email = input('Insert an e-mail: ')
-            user_password = input('Create a password: ')
+    if message is None:
+        user_email = input('Insert an e-mail: ')
+        user_password = input('Create a password: ')
 
-        if message is not None:
-            print(message)
-            user_password = input('Create a password: ')
-            pass
+    if message is not None:
+        print(message)
+        user_password = input('Create a password: ')
+        pass
 
-    def register():
+def register():
 
-        emails.append(user_email)
-        passwords.append(user_password)
-        users_shelf['email'] = emails
-        users_shelf['password'] = passwords
+    emails.append(user_email)
+    passwords.append(user_password)
+    users_shelf['email'] = emails
+    users_shelf['password'] = passwords
 
     # Strong password detector.
 
-    def password_strenght(password=None):
-        if password == '':
-            ask_user('Insert a password.')
-        else:
-            return (len(password) >= 8
-                    and password != password.upper()
-                    and password != password.lower()
-                    and any(char.isdigit() for char in password))
+def password_strenght(password=None):
+    if password == '':
+        ask_user('Insert a password.')
+    else:
+        return (len(password) >= 8
+                and password != password.upper()
+                and password != password.lower()
+                and any(char.isdigit() for char in password))
 
-    def check_strenght(strong:bool):
-        return (print('Your password is strong.') if password_strenght(user_password) is True 
-        else print('Your password isn\'t strong enough. Your password must contain at least: \
+def check_strenght(strong:bool):
+    return (print('Your password is strong.') if password_strenght(user_password) is True 
+    else print('Your password isn\'t strong enough. Your password must contain at least: \
             \n8 characters\nOne uppercase and lowercase letter\nOne digit'))
         
+if __name__ == '__main__':  
     ask_user()
     register()
     check_strenght(password_strenght(user_password))
